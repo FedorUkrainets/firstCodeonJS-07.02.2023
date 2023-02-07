64b4d171b28094ebcde0be82c3a74a8da33f24c6
FedorUkrainets <123262701+FedorUkrainets@users.noreply.github.com>
2023-02-07 21:04:08 +0300

    Add files via upload

---
+++ Object 07.02.2023.js
@@ -0,0 +1,15 @@
+// My first code on JS
+
+const CityRussia = {
+    capital: 'Moscow',
+    people: '12.000.000-people'
+}
+CityRussia.capital = 'Sait-Pesburg'
+
+CityRussia.people = '5.000.000-people'
+
+CityRussia.size = 'second size of Russia'
+
+console.log(CityRussia.capital)
+console.log(CityRussia.people)
+console.log(CityRussia.size)
